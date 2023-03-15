d26c9cd5a0ea00304cfcc08b3ac1cf04fd91a8b0
dechevgfx <101424579+dechevgfx@users.noreply.github.com>
2023-03-15 17:22:46 +0200

    pages and routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Offers from "./pages/Offers";
+import ForgotPassword from "./pages/ForgotPassword";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 function App() {
-    return (<></>);
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/offers" element={<Offers />} />
+                <Route path="/forgot-password" element={<ForgotPassword />} />
+                <Route path="/signin" element={<SignIn />} />
+                <Route path="/signup" element={<SignUp />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
