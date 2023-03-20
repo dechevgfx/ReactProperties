d9fb7dd7f5d235e0f52815902d5c057cfba7256a
Georgi Dechev <georgiddechev@gmail.com>
2023-03-20 05:10:05 -0700

    signIn + signUp basic pages

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-export default function SignIn() {
+const SignIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // TODO: handle login logic
+  }
+
   return (
-    <div>
-      IN
+    <div className="container-login">
+      <form onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" className="form-control" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" className="form-control" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        </div>
+        <button type="submit" className="btn btn-primary">Login</button>
+      </form>
     </div>
-  )
-}
+  );
+};
+
+export default SignIn;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-export default function SignUp() {
+const SignUp = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // TODO: handle register logic
+  }
+
   return (
-    <div>
-      Sign Up
+    <div className="container-login">
+      <form onSubmit={handleSubmit}>
+        <h2>Register</h2>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input type="text" className="form-control" id="name" value={name} onChange={(e) => setName(e.target.value)} required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" className="form-control" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" className="form-control" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        </div>
+        <button type="submit" className="btn btn-primary">Register</button>
+      </form>
     </div>
-  )
-}
+  );
+};
+
+export default SignUp;
